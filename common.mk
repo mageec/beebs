41ba83d21b0600c184cc3e9b96af55089cdb15f2
Ashley Whetter <awhetter.2011@my.bristol.ac.uk>
2013-07-15 12:37:42 +0100

    Made makefiles more flexible
    Only common.mk needs to be changed to add new architectures
    Also added a script for making template makefiles

--- common.mk
+++ common.mk
@@ -1,7 +1,21 @@
 PLATFORMCODE=../../lowpower-framework/platformcode
 
-CC=gcc
+VERIFY ?= 0
+ifeq ($(VERIFY), 1)
+	FLAGS += -g -DVERIFY
+endif
 
-FLAGS += -m32 -D X86
+FLAGS += -m32
 
-SRC += $(PLATFORMCODE)/jrand.c
+x86_CC=gcc
+x86_FLAGS += $(FLAGS) -DX86
+x86_SRC += $(PLATFORMCODE)/jrand.c
+
+arm_CC=arm-none-eabi-gcc
+arm_FLAGS += $(FLAGS) -DARM
+arm_FLAGS += -T$(PLATFORMCODE)/stm32vl_flash.ld
+arm_FLAGS += -mcpu=cortex-m3 -mthumb
+arm_FLAGS += -I$(PLATFORMCODE)
+arm_SRC += $(PLATFORMCODE)/{stm32f100,sbrk,exit}.c
+
+all: x86_ arm_
