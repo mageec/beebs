09983d4b7dd2535b5fc322bf402e03c46bdcab5d
James Pallister <james.pallister@bristol.ac.uk>
2014-02-04 09:27:03 +0000

    Added binary search benchmark
        * Makefile.am: Added binary search benchmark
        * Makefile.in: Regenerated
        * configure: Regenerated
        * configure.ac: Added binary search benchmark
        * src/bs/Makefile.am: Automake file for binary search
        * src/bs/Makefile.in: Regenerated
        * src/bs/bs.c: Source for binary search

---
+++ src/bs/bs.c
@@ -0,0 +1,108 @@
+/*************************************************************************/
+/*                                                                       */
+/*   SNU-RT Benchmark Suite for Worst Case Timing Analysis               */
+/*   =====================================================               */
+/*                              Collected and Modified by S.-S. Lim      */
+/*                                           sslim@archi.snu.ac.kr       */
+/*                                         Real-Time Research Group      */
+/*                                        Seoul National University      */
+/*                                                                       */
+/*                                                                       */
+/*        < Features > - restrictions for our experimental environment   */
+/*                                                                       */
+/*          1. Completely structured.                                    */
+/*               - There are no unconditional jumps.                     */
+/*               - There are no exit from loop bodies.                   */
+/*                 (There are no 'break' or 'return' in loop bodies)     */
+/*          2. No 'switch' statements.                                   */
+/*          3. No 'do..while' statements.                                */
+/*          4. Expressions are restricted.                               */
+/*               - There are no multiple expressions joined by 'or',     */
+/*                'and' operations.                                      */
+/*          5. No library calls.                                         */
+/*               - All the functions needed are implemented in the       */
+/*                 source file.                                          */
+/*                                                                       */
+/*                                                                       */
+/*************************************************************************/
+/*                                                                       */
+/*  FILE: bs.c                                                           */
+/*  SOURCE : Public Domain Code                                          */
+/*                                                                       */
+/*  DESCRIPTION :                                                        */
+/*                                                                       */
+/*     Binary search for the array of 15 integer elements.               */
+/*                                                                       */
+/*  REMARK :                                                             */
+/*                                                                       */
+/*  EXECUTION TIME :                                                     */
+/*                                                                       */
+/*                                                                       */
+/*************************************************************************/
+
+#include "platformcode.h"
+
+/* This scale factor will be changed to equalise the runtime of the
+   benchmarks. */
+#define SCALE_FACTOR    (REPEAT_FACTOR >> 0)
+
+
+struct DATA {
+  int  key;
+  int  value;
+}  ;
+
+struct DATA data[15] = { {1, 100},
+	     {5,200},
+	     {6, 300},
+	     {7, 700},
+	     {8, 900},
+	     {9, 250},
+	     {10, 400},
+	     {11, 600},
+	     {12, 800},
+	     {13, 1500},
+	     {14, 1200},
+	     {15, 110},
+	     {16, 140},
+	     {17, 133},
+	     {18, 10} };
+
+binary_search(x)
+{
+  int fvalue, mid, up, low ;
+
+  low = 0;
+  up = 14;
+  fvalue = -1 /* all data are positive */ ;
+  while (low <= up) {
+    mid = (low + up) >> 1;
+    if ( data[mid].key == x ) {  /*  found  */
+      up = low - 1;
+      fvalue = data[mid].value;
+    }
+    else  /* not found */
+      if ( data[mid].key > x ) 	{
+	up = mid - 1;
+      }
+      else   {
+             	low = mid + 1;
+      }
+  }
+  return fvalue;
+}
+
+int
+main (void)
+{
+  int i;
+
+  initialise_trigger ();
+  start_trigger ();
+
+  for (i = 0; i < SCALE_FACTOR; i++)
+    binary_search(8);
+
+  stop_trigger ();
+  return 0;
+}
