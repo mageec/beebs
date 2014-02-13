37e61cae79f4454514a7d8cf0122e378f4aa27fc
James Pallister <james.pallister@bristol.ac.uk>
2014-02-13 23:10:26 +0000

    Fixes and code to allow running on the stm32f0discovery board
        * configure: Regenerated
        * configure.ac: No longer need platform code, and change LDFLAGS to
            correct entry point
        * src/platformcode/platformcode.c: Added code to toggle PC0 of the
            stm32f0discovery board

--- src/platformcode/platformcode.c
+++ src/platformcode/platformcode.c
@@ -2,9 +2,11 @@
 
    Copyright (C) 2013  Embecosm Limited <info@embecosm.com>
 
+   Contributor James Pallister <james.pallister@embecosm.com>
    Contributor Jeremy Bennett <jeremy.bennett@embecosm.com>
 
-   This file is part of the low power benchmark
+   This file is part of the Bristol/Embecosm Energy Efficiency Benchmark
+   Suite (BEEBS)
 
    This program is free software: you can redistribute it and/or modify it
    under the terms of the GNU Lesser General Public License as published by
@@ -21,7 +23,7 @@
 
 #include "platformcode.h"
 
-#ifdef ARM
+#ifdef CORTEX_M3
 #define ADDR(x)     (*((unsigned long*)(x)))
 
 #define RCC_BASE        0x40021000
@@ -40,6 +42,41 @@ void initialise_trigger()
     GPIOC_CRL = 0x1;
 
     GPIOC_BSRR = 0x00010000;// Clear bit so pin is pulled low
+}
+
+void start_trigger()
+{
+    GPIOC_BSRR = 0x00000001;// Pull bit high
+}
+
+void stop_trigger()
+{
+    GPIOC_BSRR = 0x00010000;// bit low
+}
+
+#elif defined(CORTEX_M0)
+
+#define ADDR(x)     (*((unsigned long*)(x)))
+
+#define RCC_BASE        0x40021000
+#define RCC_AHBENR      ADDR(RCC_BASE + 0x14)
+
+#define RCC_APB2RSTR    ADDR(RCC_BASE + 0xC)
+#define RCC_APB1RSTR    ADDR(RCC_BASE + 0x10)
+#define RCC_AHBRSTR     ADDR(RCC_BASE + 0x28)
+
+#define GPIOC_BASE      0x48000800
+#define GPIOC_MODER     ADDR(GPIOC_BASE + 0x00)
+#define GPIOC_OTYPER    ADDR(GPIOC_BASE + 0x04)
+#define GPIOC_BSRR      ADDR(GPIOC_BASE + 0x18)
+
+void initialise_trigger()
+{
+    RCC_AHBENR |= 1<<19;    // Turn on GPIO C
+
+    GPIOC_MODER = 0x01;     // Set GPIOC pin 1 to output
+    GPIOC_BSRR = 0x00010000;// Clear bit so pin is pulled low
+    GPIOC_BSRR = 0x00000001;// Pull bit high
 }
 
 void start_trigger()
