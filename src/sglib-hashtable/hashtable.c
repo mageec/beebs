e837f3a2b3f1809cbb453e0ba099d25485fe268b
James Pallister <james.pallister@bristol.ac.uk>
2014-03-15 15:36:45 +0000

    Added hashtable benchmark from sglib
        * Makefile.am: Added hashtable benchmark from sglib
        * Makefile.in: Regenerated
        * configure: Regenerated
        * configure.ac: Added hashtable benchmark from sglib
        * src/sglib-hashtable/Makefile.am: Regenerated
        * src/sglib-hashtable/Makefile.in: Regenerated
        * src/sglib-hashtable/hashtable.c: Added hashtable benchmark from
            sglib
        * src/sglib-hashtable/sglib.h: Added hashtable benchmark from sglib

---
+++ src/sglib-hashtable/hashtable.c
@@ -0,0 +1,104 @@
+/* BEEBS hash benchmark
+
+   Copyright (C) 2014 Embecosm Limited and University of Bristol
+
+   Contributor James Pallister <james.pallister@bristol.ac.uk>
+
+   This file is part of the Bristol/Embecosm Embedded Energy Benchmark Suite.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
+#include "platformcode.h"
+#include <stdio.h>
+#include <string.h>
+#include "sglib.h"
+
+/* This scale factor will be changed to equalise the runtime of the
+   benchmarks. */
+#define SCALE_FACTOR    (REPEAT_FACTOR >> 0)
+
+
+#define HASH_TAB_SIZE  20
+
+typedef struct ilist {
+  int i;
+  struct ilist *next;
+} ilist;
+
+ilist *htab[HASH_TAB_SIZE];
+
+#define ILIST_COMPARATOR(e1, e2)    (e1->i - e2->i)
+
+unsigned int ilist_hash_function(ilist *e) {
+  return(e->i);
+}
+
+SGLIB_DEFINE_LIST_PROTOTYPES(ilist, ILIST_COMPARATOR, next)
+SGLIB_DEFINE_LIST_FUNCTIONS(ilist, ILIST_COMPARATOR, next)
+SGLIB_DEFINE_HASHED_CONTAINER_PROTOTYPES(ilist, HASH_TAB_SIZE, ilist_hash_function)
+SGLIB_DEFINE_HASHED_CONTAINER_FUNCTIONS(ilist, HASH_TAB_SIZE, ilist_hash_function)
+
+int array[100] = {14, 66, 12, 41, 86, 69, 19, 77, 68, 38, 26, 42, 37, 23, 17, 29, 55, 13,
+  90, 92, 76, 99, 10, 54, 57, 83, 40, 44, 75, 33, 24, 28, 80, 18, 78, 32, 93, 89, 52, 11,
+  21, 96, 50, 15, 48, 63, 87, 20, 8, 85, 43, 16, 94, 88, 53, 84, 74, 91, 67, 36, 95, 61,
+  64, 5, 30, 82, 72, 46, 59, 9, 7, 3, 39, 31, 4, 73, 70, 60, 58, 81, 56, 51, 45, 1, 6, 49,
+  27, 47, 34, 35, 62, 97, 2, 79, 98, 25, 22, 65, 71, 0};
+
+int benchmark()
+{
+  int                                   i, ai,aj, n;
+  struct ilist                          ii, *nn, *ll, *last;
+  struct sglib_hashed_ilist_iterator    it;
+  volatile int cnt = 0;
+
+  sglib_hashed_ilist_init(htab);
+
+  for (i=0; i<100; i++) {
+    ii.i = array[i];
+    if (sglib_hashed_ilist_find_member(htab, &ii) == NULL) {
+      nn = malloc(sizeof(struct ilist));
+      nn->i = array[i];
+      sglib_hashed_ilist_add(htab, nn);
+    }
+  }
+
+  last=sglib_hashed_ilist_it_init(&it,htab);
+  for(ll=sglib_hashed_ilist_it_init(&it,htab); ll!=NULL; ll=sglib_hashed_ilist_it_next(&it)) {
+      ll->i = last->i;
+      last = ll;
+  }
+
+  for(ll=sglib_hashed_ilist_it_init(&it,htab); ll!=NULL; ll=sglib_hashed_ilist_it_next(&it)) {
+      cnt += ll->i;
+      free(ll);
+  }
+
+  return cnt;
+}
+
+int
+main (void)
+{
+  int i;
+
+  initialise_trigger ();
+  start_trigger ();
+
+  for (i = 0; i < SCALE_FACTOR; i++)
+    benchmark ();
+
+  stop_trigger ();
+  return 0;
+}
+
