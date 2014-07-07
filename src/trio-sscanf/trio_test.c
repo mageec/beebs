22c5054aefb0843958a0d584cae760f22307631c
James Pallister <james.pallister@bristol.ac.uk>
2014-07-07 12:19:38 +0100

    Added trio-sscanf benchmark
        * configure: Regenerated
        * configure.ac: Added trio-sscanf benchmark
        * src/trio-sscanf/Makefile.am: Makefile.am for trio-sscanf
        * src/trio-sscanf/Makefile.in: Generated
        * src/trio-sscanf/trio.c: Library file for trio
        * src/trio-sscanf/trio.h: Library file for trio
        * src/trio-sscanf/trio_test.c: Benchmarking trio_sscanf
        * src/trio-sscanf/triodef.h: Library file for trio
        * src/trio-sscanf/triop.h: Library file for trio
        * src/trio-sscanf/triostr.c: Library file for trio
        * src/trio-sscanf/triostr.h: Library file for trio

---
+++ src/trio-sscanf/trio_test.c
@@ -0,0 +1,58 @@
+/* BEEBS template benchmark
+
+   Copyright (C) 2014 Embecosm Limited and University of Bristol
+
+   Contributor James Pallister <james.pallister@bristol.ac.uk>
+
+   This file is part of the Bristol/Embecosm Embedded Energy Benchmark Suite.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
+#include "support.h"
+#include "trio.h"
+
+/* This scale factor will be changed to equalise the runtime of the
+   benchmarks. */
+#define SCALE_FACTOR    (REPEAT_FACTOR >> 0)
+
+
+void
+benchmark (void)
+{
+  volatile int d;
+  volatile char s[20];
+
+  trio_sscanf("123", "%d", &d);
+  trio_sscanf("123 456", "%d %d", &d, &d);
+  trio_sscanf("000000123", "%d", &d);
+  trio_sscanf("abcdefg", "%x", &d);
+  trio_sscanf("FF", "%x",&d);
+  trio_sscanf("abcdefg", "%[^f]%xg", s, &d);
+}
+
+
+int
+main (void)
+{
+  int i;
+
+  initialise_board ();
+  start_trigger ();
+
+  for (i = 0; i < SCALE_FACTOR; i++)
+    benchmark ();
+
+  stop_trigger ();
+  return 0;
+}
