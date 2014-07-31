817b2ba1a86e3a614d730ac750fb34c762488bfc
Andrew Burgess <andrew.burgess@embecosm.com>
2014-07-31 20:18:42 +0100

    Fix build warnings in crc.
    Undefined code, and unused variables.
    
    ChangeLog:
    
    	* src/crc/crc.c (icrc1): Rewrite undefined code.
    	(benchmark): Discard unused variable by casting to void.

--- src/crc/crc.c
+++ src/crc/crc.c
@@ -88,7 +88,10 @@ unsigned short icrc1(unsigned short crc, unsigned char onech)
 
 	for (i=0;i<8;i++) {
 		if (ans & 0x8000)
-			ans = (ans <<= 1) ^ 4129;
+		  {
+			ans <<= 1;
+			ans = ans ^ 4129;
+		  }
 		else
 			ans <<= 1;
 	}
@@ -138,13 +141,16 @@ unsigned short icrc(unsigned short crc, unsigned long len,
 
 int benchmark(void)
 {
-
   unsigned short i1,i2;
   unsigned long n;
 
   n=40;
   i1=icrc(0,n,(short)0,1);
   i2=icrc(i1,n+2,(short)0,1);
+
+  /* Silence compiler warning about unused variables.  */
+  (void) i2;
+
   return 0;
 }
 
