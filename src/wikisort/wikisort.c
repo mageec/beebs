64a6ad9e92670328a6fe6e9ffaded207e17471a4
Andrew Burgess <andrew.burgess@embecosm.com>
2014-08-01 19:23:56 +0100

    Fix compiler warnings in test wikisort.
    Unused local variables.
    
    ChangeLog:
    
    	* src/wikisort/wikisort.c (benchmark): Remove unused variables.

--- src/wikisort/wikisort.c
+++ src/wikisort/wikisort.c
@@ -685,7 +685,6 @@ Test array1[400];
 
 int benchmark() {
 	long total, index, test_case;
-	double total_time, total_time1, total_time2;
 	Comparison compare = TestCompare;
 
 	__typeof__(&TestingPathological) test_cases[] = {
