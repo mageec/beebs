3edf20893b558587f7a5ae013c6eb95d2f336e2a
Andrew Burgess <andrew.burgess@embecosm.com>
2014-08-11 17:03:04 +0100

    Add GPL header to the miniz test.
    ChangeLog:
    
    	* src/miniz/miniz.c: Add GPL header.
    	* src/miniz/miniz.h: Add GPL header.

--- src/miniz/miniz.c
+++ src/miniz/miniz.c
@@ -1,3 +1,20 @@
+/* BEEBS miniz benchmark.
+
+   This file is part of the Bristol/Embecosm Embedded Benchmark Suite.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
 /* miniz.c v1.15 - public domain deflate/inflate, zlib-subset, ZIP reading/writing/appending, PNG writing
    See "unlicense" statement at the end of this file.
    Rich Geldreich <richgel99@gmail.com>, last updated Oct. 13, 2013
