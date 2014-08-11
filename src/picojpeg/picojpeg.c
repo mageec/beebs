4289e4fbf2eee654db0ac7f6c67c07b393489eee
Andrew Burgess <andrew.burgess@embecosm.com>
2014-08-11 17:42:28 +0100

    Add GPL header to the picojpeg test source.
    This source is public domain, the BEEBS version is GPL.
    
    ChangeLog:
    
    	* src/picojpeg/picojpeg.c: Add GPL header.
    	* src/picojpeg/picojpeg.h: Add GPL header.

--- src/picojpeg/picojpeg.c
+++ src/picojpeg/picojpeg.c
@@ -1,3 +1,18 @@
+/* This file is part of the Bristol/Embecosm Embedded Benchmark Suite.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
 //------------------------------------------------------------------------------
 // picojpeg.c v1.1 - Public domain, Rich Geldreich <richgel99@gmail.com>
 // Nov. 27, 2010 - Initial release
