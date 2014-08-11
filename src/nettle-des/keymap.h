02a74209f4224fb163fcec748730d2486db1cc02
Andrew Burgess <andrew.burgess@embecosm.com>
2014-08-11 17:07:18 +0100

    Add GPL header to two header files in nettle-des test.
    ChangeLog:
    
    	* src/nettle-des/keymap.h: Add GPL header.
    	* src/nettle-des/rotors.h: Add GPL header.

--- src/nettle-des/keymap.h
+++ src/nettle-des/keymap.h
@@ -1,3 +1,20 @@
+/* BEEBS nettle-des benchmark
+
+   This file is part of the Bristol/Embecosm Embedded Benchmark Suite.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
 /* automagically made - do not fuss with this */
 
  0x02080008, 0x02082000, 0x00002008, 0x00000000,
