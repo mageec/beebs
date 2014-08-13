d2adeb99bbc9bd6ea8d7a21f5684e754463f073d
Andrew Burgess <andrew.burgess@embecosm.com>
2014-08-13 17:39:14 +0100

    Add GPL and original licence header to nbody benchmark.
    The original licence details for the nbody benchmark had not been
    committed along with the source.  I've added the original licence
    details back into the source file, and added a GPL header as I believe
    this is acceptable.
    
    ChangeLog:
    
    	* src/nbody/nbody.c: Add GPL header, and original licence and
    	copyright information.

--- src/nbody/nbody.c
+++ src/nbody/nbody.c
@@ -1,3 +1,67 @@
+/* BEEBS nbody benchmark
+
+   Copyright (C) 2014 Embecosm Limited and University of Bristol
+
+   This file is part of the Bristol/Embecosm Embedded Benchmark Suite.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>. */
+
+/* The original source code for this benchmark can be found here:
+
+     http://benchmarksgame.alioth.debian.org/
+
+   and was released under the following licence, disclaimers, and
+   copyright:
+
+   Revised BSD license
+
+   This is a specific instance of the Open Source Initiative (OSI) BSD
+   license template http://www.opensource.org/licenses/bsd-license.php
+
+   Copyright 2004-2009 Brent Fulgham
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+   Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the
+   distribution.
+
+   Neither the name of "The Computer Language Benchmarks Game" nor the
+   name of "The Computer Language Shootout Benchmarks" nor the names
+   of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written
+   permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+   STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+   OF THE POSSIBILITY OF SUCH DAMAGE.  */
 
 #include <math.h>
 #include <stdlib.h>
