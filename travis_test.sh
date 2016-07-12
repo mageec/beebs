aa2c1d881153090f00ea936d39bd22d3a09f0c8f
Graham Markall <graham.markall@embecosm.com>
2016-07-12 16:21:59 +0100

    Adjusr fold name

--- travis_test.sh
+++ travis_test.sh
@@ -5,12 +5,12 @@ make || exit 1
 make check
 res=$?
 
-echo travis_fold:start:#beebs.log
+echo travis_fold:start:beebs.log
 cat beebs.log
-echo travis_fold:end:#beebs.log
+echo travis_fold:end:beebs.log
 
-echo travis_fold:start:#beebs.sum
+echo travis_fold:start:beebs.sum
 cat beebs.sum
-echo travis_fold:end:#beebs.sum
+echo travis_fold:end:beebs.sum
 
 exit $res
