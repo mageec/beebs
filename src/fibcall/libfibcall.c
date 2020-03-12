0eedeac59e85ca5cf5ba7a76caa0940808d3b449
Kyriakos Georgiou <kg8280@bristol.ac.uk>
2020-03-12 22:47:50 +0000

    removing uneeded parenthesis

--- src/fibcall/libfibcall.c
+++ src/fibcall/libfibcall.c
@@ -24,7 +24,7 @@
 
 /* This scale factor will be changed to equalise the runtime of the
    benchmarks. */
-#define SCALE_FACTOR    (REPEAT_FACTOR << 5))
+#define SCALE_FACTOR    (REPEAT_FACTOR << 5)
 
 /* $Id: fibcall.c,v 1.2 2005/04/04 11:34:58 csg Exp $ */
 
